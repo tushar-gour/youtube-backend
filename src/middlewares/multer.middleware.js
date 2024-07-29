a1be935b26e037d34aa67413b9781da703e097c6
tushar-gour <tushargour004@gmail.com>
2024-07-30 01:08:34 +0530

    all setup completed

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+
+    filename: function (req, res, cb) {
+        cb(null, file.originalname);
+    },
+});
+
+export const upload = multer({
+    storage,
+});
