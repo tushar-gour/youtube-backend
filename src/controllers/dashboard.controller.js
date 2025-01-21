2a41ddde690a39d5dbf98304e2687fa47ce74787
tushar-gour <tushargour004@gmail.com>
2025-01-22 00:54:11 +0530

    completed dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -7,11 +7,54 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    // TODO: Get the channel stats like total total subscribers, video views, total videos, total likes etc.
+    const { channel } = req.params;
+    const { page = 1, limit = 10 } = req.query;
+
+    if (!mongoose.Types.ObjectId.isValid(channel)) {
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    const totalSubscribers = await Subscription.find({ channel })
+        .skip((page - 1) * limit)
+        .limit(parseInt(limit))
+        .countDocuments({ channel });
+
+    const totalVideos = await Video.countDocuments({ owner: channel });
+
+    const totalViews = await Video.aggregate([
+        { $match: { owner: channel } },
+        { $group: { _id: null, totalViews: { $sum: "$views" } } },
+    ]);
+
+    const totalLikes = await Like.countDocuments({ channel: channel });
+
+    return res.status(201).json(
+        new ApiResponse(
+            200, 
+            {
+                totalSubscribers,
+                totalVideos,
+                totalViews: totalViews[0]?.totalViews || 0,
+                totalLikes,
+            },
+            "Data fetched successfully"
+        )
+    );
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const { channel } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(channel)) {
+        throw new ApiError(400, "Invalid channel ID");
+    }
+    const totalVideos = await Video.countDocuments({ owner: channel });
+
+    return res.status(201).json(
+        new ApiResponse(201, totalVideos, "Videos fetched successfully")
+    );
 });
 
 export { getChannelStats, getChannelVideos };
